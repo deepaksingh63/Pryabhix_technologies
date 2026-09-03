95229f22c0650e495f059e6f14a6df6bc8a82a6a
deepaksingh63 <abhisheksingh6390758373@gmail.com>
2026-09-03 09:25:08 +0530

    Add Gorakhpur website development SEO page

--- webcraft-react/src/App.jsx
+++ webcraft-react/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
+
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+
 import Home from './pages/Home.jsx'
 import Services from './pages/Services.jsx'
 import Portfolio from './pages/Portfolio.jsx'
@@ -9,12 +11,15 @@ import About from './pages/About.jsx'
 import Process from './pages/Process.jsx'
 import Testimonials from './pages/Testimonials.jsx'
 import Contact from './pages/Contact.jsx'
+import WebsiteDevelopmentGorakhpur from './pages/WebsiteDevelopmentGorakhpur.jsx'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [pathname])
+
   return null
 }
 
@@ -22,9 +27,20 @@ export default function App() {
   return (
     <>
       <ScrollToTop />
+
       <Navbar />
+
       <Routes>
+        {/* Homepage */}
         <Route path="/" element={<Home />} />
+
+        {/* SEO Landing Page */}
+        <Route
+          path="/website-development-gorakhpur"
+          element={<WebsiteDevelopmentGorakhpur />}
+        />
+
+        {/* Main Pages */}
         <Route path="/services" element={<Services />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/about" element={<About />} />
@@ -32,6 +48,7 @@ export default function App() {
         <Route path="/testimonials" element={<Testimonials />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
     </>
   )
